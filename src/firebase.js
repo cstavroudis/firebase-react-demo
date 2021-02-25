e50fe03a5ff414cb64155353787d4c18efd977a5
Calandra Stavroudis <calandra.st@gmail.com>
2021-02-25 12:00:33 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyDadhQvQJsezb0Jj8LkaA6NPHvZ6b3guuY",
+  authDomain: "fun-food-friends-eeec7.firebaseapp.com",
+  databaseURL: "https://fun-food-friends-eeec7.firebaseio.com",
+  projectId: "fir-react-demo-8e3cd",
+  storageBucket: "fun-food-friends-eeec7.appspot.com",
+  messagingSenderId: "144750278413",
+};
+firebase.initializeApp(config);
+export default firebase;
